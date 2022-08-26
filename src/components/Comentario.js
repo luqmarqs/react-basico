f3d3ac4c1b6984e6e80ab57ec4b3d660b797cb90
Lucas Marques <66765396+luqmarqs@users.noreply.github.com>
2022-08-25 21:41:07 -0300

    Criando o projeto

---
+++ src/components/Comentario.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Comentario.css'
+//iniciar componentes com letra maiuscula
+const Comentario = () => (
+    <div className='Comentario'>
+        <div>
+            João:
+        </div>
+        <div>
+            Olá, tudo bem?
+        </div>
+    </div>
+);
+
+export default Comentario;
